8fd6be2b875249c7601d416f66a022866c82cad8
Ilia Koliaskin <kolyaskin.ilya@gmail.com>
2024-12-01 12:15:53 +0100

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,30 @@
+import kotlin.math.abs
+
 fun main() {
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
+        return input.map { it.split("   ") }
+            .map { Pair(it[0].toInt(), it[1].toInt()) }
+            .unzip()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val unzip = parseInput(input)
+
+        val first = unzip.first.sorted()
+        val second = unzip.second.sorted()
+
+        return first.mapIndexed { i, elem -> abs(elem - second[i]) }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val unzip = parseInput(input)
+        val secondByCount = unzip.second.groupingBy { it }.eachCount()
+        return unzip.first.sumOf { it * (secondByCount[it] ?: 0) }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
